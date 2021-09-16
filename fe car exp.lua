16973c81456a93eecc512e0eab40dc8085e3b74d
Freebob_ux228 <90867201+Freebobux228rblx@users.noreply.github.com>
2021-09-16 23:15:48 +0300

    Fe car experimental version

---
+++ fe car exp.lua
@@ -0,0 +1,199 @@
+local ScreenGui = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local ImageLabel = Instance.new("ImageLabel")
+local TextLabel = Instance.new("TextLabel")
+local EzModsLogo = Instance.new("ImageLabel")
+
+
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+ 
+Frame.Parent = ScreenGui
+Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame.BackgroundTransparency = 1.000
+Frame.BorderColor3 = Color3.fromRGB(27, 42, 53)
+Frame.BorderSizePixel = 0
+Frame.Position = UDim2.new(0.462835729, 0, 0.339066327, 0)
+Frame.Size = UDim2.new(0, 200, 0, 200)
+ 
+ImageLabel.Parent = Frame
+ImageLabel.BackgroundColor3 = Color3.fromRGB(52, 52, 52)
+ImageLabel.BackgroundTransparency = 1.000
+ImageLabel.Position = UDim2.new(-4.65499878, 0, 0.380000025, 0)
+ImageLabel.Size = UDim2.new(0, 200, 0, 200)
+ImageLabel.Image = "http://www.roblox.com/asset/?id=134328961"
+ 
+TextLabel.Parent = ImageLabel
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.Position = UDim2.new(0, 0, 0.694999993, 0)
+TextLabel.Size = UDim2.new(0, 200, 0, 19)
+TextLabel.Font = Enum.Font.SourceSans
+TextLabel.Text = "Searching Fe car experiment ver..."
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.TextSize = 14.000
+ 
+EzModsLogo.Name = "EzModsLogo"
+EzModsLogo.Parent = ImageLabel
+EzModsLogo.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+EzModsLogo.BackgroundTransparency = 1.000
+EzModsLogo.Position = UDim2.new(0.0610494167, 0, 0.193213031, 0)
+EzModsLogo.Size = UDim2.new(0, 174, 0, 100)
+EzModsLogo.ZIndex = 4
+EzModsLogo.Image = "rbxassetid://4884056983"
+ 
+ImageLabel:TweenPosition(UDim2.new(-0.205, 0,0.155, 0), Enum.EasingDirection.Out)wait(0.1)
+wait(0.9)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+TextLabel.Text = "w"
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+TextLabel.Text = "wa"
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+TextLabel.Text = "wai"
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+TextLabel.Text = "wait"
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+TextLabel.Text = "waiti"
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+TextLabel.Text = "waitin"
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+TextLabel.Text = "waiting"
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+TextLabel.Text = "waiting "
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+TextLabel.Text = "waiting f"
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+TextLabel.Text = "waiting fo"
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+TextLabel.Text = "waiting for "
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+TextLabel.Text = "waiting for a"
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+TextLabel.Text = "waiting for a"
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+TextLabel.Text = "waiting for a "
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+TextLabel.Text = "waiting for a r"
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+TextLabel.Text = "waiting for a re"
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+TextLabel.Text = "waiting for a resp"
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+TextLabel.Text = "waiting for a respo"
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+TextLabel.Text = "waiting for a respon"
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+TextLabel.Text = "waiting for a respons"
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+TextLabel.Text = "waiting for a response"
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+TextLabel.Text = "waiting for a response."
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+TextLabel.Text = "waiting for a response.."
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+TextLabel.Text = "waiting for a response..."
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+ EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+ EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+wait(0.1)
+EzModsLogo.Rotation = EzModsLogo.Rotation + 0.5
+wait(0.1)
+
+ImageLabel:TweenPosition(UDim2.new(5, 0,0.155, 0), Enum.EasingDirection.Out)
+while wait() do
+    if ImageLabel.Position == UDim2.new(5, 0,0.155, 0) then
+        ScreenGui:Destroy()
+        break
+    end
+end
+
+game:GetService("StarterGui"):SetCore("SendNotification",{
+            Title = "Fe car experimental version";
+            Text = "Fe car experiment version is not working";
+                    })
