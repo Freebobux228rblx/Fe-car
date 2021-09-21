deaceaf3546c9737de1dad0c2e4ccccfb23ae197
Freebob_ux228 <90867201+Freebobux228rblx@users.noreply.github.com>
2021-09-21 16:25:27 +0300

    1.12.1
    1. An error was found in "Enable script" 
    2. "Copy #2" was added in the "Discord links" section  
    3. The script name has been changed 
    4. The Gui section has been added

--- Fecar.lua
+++ Fecar.lua
@@ -11,18 +11,22 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Regul
  
 --GUI
 local Gui = Library:AddGui({
-    Title = {"FE Car script", "Made by Freebob_ux228#1814 and HtB#1526."},
+    Title = {"FE Car script", "Made by Freebob_ux228 and HtB"},
     ThemeColor = Color3.fromRGB(0,250,250),
     ToggleKey = Enum.KeyCode.RightShift,
 })
  
+local Tab = Gui:AddTab("Gui")
+local Category = Tab:AddCategory("Minimize")
+local Label = Category:AddLabel("Press right shift")
+
 local Tab = Gui:AddTab("Main")
-local Category = Tab:AddCategory("Main `version: 1.11.7` LOADED FROM GITHUB")
+local Category = Tab:AddCategory("Main `version: 1.12.1` LOADED FROM GITHUB")
 local Label = Category:AddLabel("Working only on R6. Not working on r15.")
  
 --Enable script
 local Button = Category:AddButton("Enable Car script (90km/h)", function()
-    LocalPlayer.Character.Humanoid.WalkSpeed=90 LocalPlayer.Character.Humanoid.JumpPower=1 Float_Height="-1.03" AnimationId="129342287" local a=Instance.new("Animation") a.AnimationId="rbxassetid://"..AnimationId local a=LocalPlayer.Character.Humanoid:LoadAnimation(a) a:Play() a:AdjustSpeed(1) for a,a in pairs(LocalPlayer.Character:GetDescendants())do if a.ClassName=="Part"then a.CustomPhysicalProperties=PhysicalProperties.new(0,0,0)end end local a=1 G=game _=wait p=G:GetService("Players").LocalPlayer.Character p:FindFirstChild("Humanoid").HipHeight=Float_Height _(1.5) t=.4 for a=1,a do repeat p:FindFirstChild("Humanoid").HipHeight=Float_Height-n _(t) p:FindFirstChild("Humanoid").HipHeight=Float_Height+n _(t)until p:FindFirstChild("Humanoid").Health==0 end
+    LocalPlayer.Character.Humanoid.WalkSpeed=17 LocalPlayer.Character.Humanoid.JumpPower=1 Float_Height="-1.03" AnimationId="129342287" local a=Instance.new("Animation") a.AnimationId="rbxassetid://"..AnimationId local a=LocalPlayer.Character.Humanoid:LoadAnimation(a) a:Play() a:AdjustSpeed(1) for a,a in pairs(LocalPlayer.Character:GetDescendants())do if a.ClassName=="Part"then a.CustomPhysicalProperties=PhysicalProperties.new(0,0,0)end end local a=1 G=game _=wait p=G:GetService("Players").LocalPlayer.Character p:FindFirstChild("Humanoid").HipHeight=Float_Height _(1.5) t=.4 for a=1,a do repeat p:FindFirstChild("Humanoid").HipHeight=Float_Height-n _(t) p:FindFirstChild("Humanoid").HipHeight=Float_Height+n _(t)until p:FindFirstChild("Humanoid").Health==0 end
 end)
  
 --Height
@@ -79,11 +83,35 @@ spawn(function()
 end)
 end)
  
+local Button = Category:AddButton("Copy #2", function()
+    setclipboard("HtB#1526")
+    Library:Notify("Check your clipboard")
+    local json = {
+   ["cmd"] = "INVITE_BROWSER",
+   ["args"] = {
+       ["code"] = "1526"
+   },
+   ["nonce"] = 'a'
+}
+ 
+spawn(function()
+   print(syn.request({
+       Url = 'http://127.0.0.1:6463/rpc?v=1',
+       Method = 'POST',
+       Headers = {
+           ['Content-Type'] = 'application/json',
+           ['Origin'] = 'https://discord.com'
+       },
+       Body = game:GetService('HttpService'):JSONEncode(json),
+   }).Body)
+end)
+end)
+
 --Close gui
 local Tab = Gui:AddTab("Close")
 local Category = Tab:AddCategory("Close")
 local Button = Category:AddButton("Close GUI", function()
-    Core["FE Car script Made by Freebob_ux228#1814 and HtB#1526."]:Destroy()
+    Core["FE Car script Made by Freebob_ux228 and HtB"]:Destroy()
 end)
  
 --Notif
