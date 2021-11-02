1ed49c59cde84b5717a3f722cea69a61315ee94f
Freebob_ux228 <90867201+Freebobux228rblx@users.noreply.github.com>
2021-11-02 11:19:24 +0300

    Update Fecar.lua

--- Fecar.lua
+++ Fecar.lua
@@ -1,6 +1,1 @@
-game:GetService("StarterGui"):SetCore("SendNotification",{
-            Title = "Notif";
-            Text = "I obfuscated code, sorry. Made by Freebob_ux228#1814 and HtB#1526";
-                    })
-                    wait(5.5)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Freebobux228rblx/Fe-car/main/Fecar-obfuscated.lua"))()
